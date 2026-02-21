81fbc8a75d826a3f22d7a1559190979a18f1c217
Бог <manshadow834@gmail.com>
2026-02-21 14:37:42 +0100

    HW07: Move state management to Homework_07 component

---
+++ src/lessons/Lesson_07/Lesson_07.tsx
@@ -0,0 +1,54 @@
+import "./styles.css";
+import { WEATHER_CODES } from "./types";
+// !! Есть 2 основных способа вызова перерендера(обновления) компонента:
+// 1 - изменения state(посредством вызова функции setState())
+// 2 - изменение props
+function Lesson_07() {
+  // 12. enum
+  // Создайте функцию, которая бы принимала следующие параметры:
+  //   код погоды и функция decode, которая дает расшифровку погоды по коду.
+  //   Функция должна возвращать строку, описывающую погоду. Таблица соответствия
+  //   код-описание: SQ – шквал PO – пыльный вихрь FC - торнадо BR – дымка
+  //   (видимость от 1 до 9 км) HZ – мгла (видимость менее 10 км) FU – дым
+  //   (видимость менее 10 км) DS - пыльная буря (видимость менее 10 км) SS -
+  //   песчаная буря (видимость менее 10 км) Подсказка: удобно использовать в
+  //   одном из методов switch-case:
+  const decode = (weatherCode: string): string => {
+    switch (weatherCode) {
+      case WEATHER_CODES.SQ: {
+        return "шквал";
+      }
+      case WEATHER_CODES.PO: {
+        return "пыльный вихрь";
+      }
+      case WEATHER_CODES.FC: {
+        return "торнадо";
+      }
+      case WEATHER_CODES.BR: {
+        return "дымка (видимость от 1 до 9 км)";
+      }
+      case WEATHER_CODES.HZ: {
+        return "мгла (видимость менее 10 км)";
+      }
+      case WEATHER_CODES.FU: {
+        return "дым (видимость менее 10 км)";
+      }
+      case WEATHER_CODES.DS: {
+        return "пыльная буря (видимость менее 10 км)";
+      }
+      case WEATHER_CODES.SS: {
+        return "песчаная буря (видимость менее 10 км) ";
+      }
+      default:
+        return "This code doesn't exist";
+    }
+  };
+  const weather = decode(WEATHER_CODES.PO);
+  console.log(weather);
+
+  ///////////////////////////////////////////////////////////////////////////////
+  // 13. Generic
+  
+  return <div className="lesson_07_page_wrapper"></div>;
+}
+export default Lesson_07;
